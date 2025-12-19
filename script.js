117f8fae4ef0b177ac87da7fffae98b16a5912a1
Alan <alannajera3319@gmail.com>
2025-12-19 14:07:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const quotes = [
   {
     quote: "This is a test quote.",
     explanation: "This is a test explanation to make sure the button works."
+  },
+  {
+    quote: "Nothing kills a man faster than his own head ~Trapdoor (Self Titled, 2009)",
+    explanation: "We're the only ones standing in the way of everything we want."
   }
 ];
+
 
 // Get references to HTML elements
 const button = document.getElementById("quoteBtn");
